3246d10fe187cccbd68a74a2895ad0d1ddab445f
João Gomes <joaosilgo96@gmail.com>
2020-07-12 04:18:13 +0100

    Updates

---
+++ precache-manifest.7292718929e365df500212787107bc49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38bfed2c0569e838b0e5cd76597c1951",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "a674c4ca1c4ef2170a64",
+    "url": "/react-app/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "03846d6d8263c90e5a6c",
+    "url": "/react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a674c4ca1c4ef2170a64",
+    "url": "/react-app/static/js/2.494ea925.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/react-app/static/js/2.494ea925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03846d6d8263c90e5a6c",
+    "url": "/react-app/static/js/main.fcc739db.chunk.js"
+  },
+  {
+    "revision": "e2c6b0a16d423a796dce",
+    "url": "/react-app/static/js/runtime-main.9a102076.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.f225daedbdeb6752487c58b53e621b38.js"
+  "/react-app/precache-manifest.7292718929e365df500212787107bc49.js"
 );
 
 self.addEventListener('message', (event) => {
